85e792d335ef7cc14e004dec621455be56a1e18c
karimi <behrouzkarimi1993@gmail.com>
2025-07-10 23:06:17 +0100

    added to run Shape objects

---
+++ src/main/java/training/exercises/exercise0/ShapesRunner.java
@@ -0,0 +1,11 @@
+package training.exercises.exercise0;
+
+public class ShapesRunner {
+    
+    public static void main(String[] args){
+
+        Shapes shape=new Shapes();
+
+        shape.printASquare();
+    }
+}
