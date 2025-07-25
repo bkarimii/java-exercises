999b05e636435ca6bd697c9139a08b7820ecd8ee
karimi <behrouzkarimi1993@gmail.com>
2025-07-25 15:24:44 +0100

    user stroy 2 implemented

--- src/main/java/training/miniproject/BankingApplication.java
+++ src/main/java/training/miniproject/BankingApplication.java
@@ -5,13 +5,13 @@ import java.util.Map;
 /**
  * An interactive command-line banking application.
  * <p>
- * This will be implemented as part of the mini-project which wraps up the Intro to Java
+ * This will be implemented as part of the mini-project which wraps up the Intro
+ * to Java
  * training course.
  * <p>
  * See README.md in this package for instructions!
  */
 public class BankingApplication {
-
 
   /**
    * The entry point to our command-line banking application.
@@ -21,15 +21,23 @@ public class BankingApplication {
     Map<Integer, Integer> accountNumbersToOpeningBalances = Map.of(
         1, 100,
         2, 10_000,
-        3, -500
-    );
+        3, -500);
 
-    // Do this in an infinite loop so that the application keeps running after a user is finished,
+    // Do this in an infinite loop so that the application keeps running after a
+    // user is finished,
     // and other users can sign in and use the banking application.
     while (true) {
       int accountNumber = UserInput.getInteger("Please enter your account number: ");
+      Integer balance = accountNumbersToOpeningBalances.get(accountNumber);
+      if (balance != null) {
+        System.out.println("Welcome, " + accountNumber + "!");
+        System.out.println("This is your balance: " + balance);
+      } else {
+        System.out.println(
+            "\n Account number wasn't found! please eneter a correct banck account number!\n if you don't have an account open one first.");
+        continue;
+      }
 
-      System.out.println("Welcome, " + accountNumber + "!");
     }
   }
 }
